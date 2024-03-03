cee9807ee2bed297d9d3ee5f3cd350f36da2b05e
Haylea M <137733720+HayHay1992@users.noreply.github.com>
2024-03-03 14:17:05 -0500

    Update urls.py

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]

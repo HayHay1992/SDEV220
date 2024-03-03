f7f423a552b3f7f341d506199babf3814c235fa5
Haylea M <137733720+HayHay1992@users.noreply.github.com>
2024-03-03 14:30:54 -0500

    Update models.py

--- django/blog/models.py
+++ django/blog/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    
+    def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
 
     def publish(self):
         self.published_date = timezone.now()

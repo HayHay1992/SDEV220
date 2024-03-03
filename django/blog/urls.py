20c8afc42eb00a6733146d25481e608c89d6b5f2
Haylea M <137733720+HayHay1992@users.noreply.github.com>
2024-03-03 14:20:26 -0500

    Update urls.py

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+        path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('', include('blog.urls')),
 ]

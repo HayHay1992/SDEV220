feaee203a42197678067f58c8a3d3c05a6b0c265
Haylea M <137733720+HayHay1992@users.noreply.github.com>
2024-03-03 14:14:01 -0500

    Update views.py

--- django/blog/views.py
+++ django/blog/views.py
@@ -5,3 +5,8 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
